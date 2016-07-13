0b49db3f5aaa3288fffb64a9cbcbdf13ef094e93
Romilly Cocking <romilly.cocking@gmail.com>
2016-07-13 08:53:26 +0100

    Added initial planning mind-map

---
+++ plan/Raspberry-Pi-workshop-plan.mm
@@ -0,0 +1,137 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="Freemind_Link_1331878192" CREATED="1266488194781" MODIFIED="1468395694633"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Raspberry Pi
+    </p>
+    <p style="text-align: center">
+      workshop
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Goal" POSITION="right" ID="Freemind_Link_1242386705" CREATED="1266488217621" MODIFIED="1266488568255" COLOR="#37a3cb">
+<node TEXT="deliver a short workshop" ID="ID_1823868314" CREATED="1468396163346" MODIFIED="1468396283904"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Cannot assume keyboards/screens, can assume laptops + Pis.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Introduction to the Pi" ID="ID_251262403" CREATED="1468396277123" MODIFIED="1468396280062"/>
+<node TEXT="hardware" ID="ID_1902315949" CREATED="1468396288563" MODIFIED="1468396292064">
+<node TEXT="keyboards/screens" ID="ID_1411816832" CREATED="1468396310840" MODIFIED="1468396317502">
+<icon BUILTIN="stop-sign"/>
+</node>
+<node TEXT="laptops" ID="ID_818715305" CREATED="1468396320011" MODIFIED="1468396325678">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Raspberry Pis" ID="ID_1121747044" CREATED="1468396342810" MODIFIED="1468396348751"/>
+</node>
+</node>
+</node>
+<node TEXT="Plan" POSITION="right" ID="_" CREATED="1266488212530" MODIFIED="1266488546784" COLOR="#373dcb">
+<edge COLOR="#808080"/>
+</node>
+<node TEXT="Tasks" POSITION="right" ID="Freemind_Link_857865306" CREATED="1266488223477" MODIFIED="1266488636684" COLOR="#de5613"/>
+<node TEXT="Review" POSITION="left" ID="Freemind_Link_1153466861" CREATED="1266488259116" MODIFIED="1266488895851" COLOR="#fb1802">
+<icon BUILTIN="xmag"/>
+<node TEXT="WW" ID="Freemind_Link_1466796877" CREATED="1266488965666" MODIFIED="1296303686570" COLOR="#ffcc00" BACKGROUND_COLOR="#006666">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="WDNW" ID="Freemind_Link_1910608691" CREATED="1266488980018" MODIFIED="1296303689746">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node TEXT="WDD" ID="Freemind_Link_1294877740" CREATED="1266488984697" MODIFIED="1296303577864" COLOR="#00cc00">
+<icon BUILTIN="licq"/>
+</node>
+<node TEXT="Questions" ID="Freemind_Link_1003502265" CREATED="1266489024048" MODIFIED="1296303570801" COLOR="#00cccc">
+<icon BUILTIN="help"/>
+</node>
+<node TEXT="Surprises" ID="Freemind_Link_19217072" CREATED="1266489032944" MODIFIED="1296303119998" COLOR="#ff0000" BACKGROUND_COLOR="#ffff00">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node TEXT="Feelings" ID="Freemind_Link_1706517660" CREATED="1266489043119" MODIFIED="1296303692921" COLOR="#cc0000">
+<icon BUILTIN="ksmiletris"/>
+</node>
+<node TEXT="Ideas" ID="Freemind_Link_1517433488" CREATED="1266488273147" MODIFIED="1266488921179" COLOR="#36d12a">
+<icon BUILTIN="idea"/>
+</node>
+<node TEXT="Fridge" ID="Freemind_Link_1954142386" CREATED="1266488793745" MODIFIED="1266488959040" COLOR="#9679df">
+<icon BUILTIN="desktop_new"/>
+</node>
+</node>
+<node TEXT="Record" POSITION="left" ID="Freemind_Link_813012694" CREATED="1266488248876" MODIFIED="1266488728945" COLOR="#27444b">
+<icon BUILTIN="pencil"/>
+</node>
+<node TEXT="Resources" POSITION="left" ID="Freemind_Link_1594441695" CREATED="1273319832507" MODIFIED="1296303609303" COLOR="#33ff33">
+<icon BUILTIN="password"/>
+<node TEXT="http://www.makeuseof.com/tag/run-remote-desktop-raspberry-pi-vnc/" ID="ID_1074668852" CREATED="1468395770846" MODIFIED="1468395770846" LINK="http://www.makeuseof.com/tag/run-remote-desktop-raspberry-pi-vnc/"/>
+<node TEXT="https://support.apple.com/kb/DL999?locale=en_US" ID="ID_512409514" CREATED="1468395818225" MODIFIED="1468395818225" LINK="https://support.apple.com/kb/DL999?locale=en_US"/>
+</node>
+</node>
+</map>
